ead93cdb972387344afe269c9d0bb8a0081593b8
Ali Valipour <ali.v@780.ir>
2023-12-09 21:16:16 +0330

    feat: add Exo2 font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Exo+2:wght@100;200;300;400;500;600;700;800;900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
         <Main />
         <NextScript />
       </body>
